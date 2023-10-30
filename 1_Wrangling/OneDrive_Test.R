e860f1401193e3f369171d49839e9e323250a904
Eva Phillips <60586613+evaphillips@users.noreply.github.com>
2023-10-30 13:44:31 -0400

    Update OneDrive_Test.R

--- 1_Wrangling/OneDrive_Test.R
+++ 1_Wrangling/OneDrive_Test.R
@@ -4,7 +4,7 @@
 # AUTHOR: Eva Phillips
 # DATE: 2023-10-30
 
-# PURPOSE: Test read/write to OneDrive shares folder
+# PURPOSE: Test read/write to OneDrive shared folder
 
 # Setup ------------------------------------------------------------------------
 library(tidyverse)
@@ -13,15 +13,16 @@ library(grid)
 library(gridExtra)
 library(readxl)
 
+#adjust to reflect your file path
 path = file.path("/Users", "evaphillips", "Library/CloudStorage/OneDrive-nyu.edu/right-to-counsel-capstone-data/01_Eviction/eviction_tracking_system", fsep="/")
-
 setwd(path)
 
 
 # Test ------------------------------------------------------------------------
 
+#read in a dataset
 ets_all_sites <- read.csv("all_sites_monthly_2020_2021.csv")
-
+#filter
 ets_houston <- ets_all_sites %>% filter(city == "Houston, TX")
-
+#write csv to file path
 write_csv(ets_houston, "ets_houston-TEST.csv")
