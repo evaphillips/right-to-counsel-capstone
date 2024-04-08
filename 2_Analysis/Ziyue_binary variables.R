c42ea41d70722edca3f38e71b83bd4d1f0ea312b
Yuliazzy <119341302+Yuliazzy@users.noreply.github.com>
2024-04-08 13:04:30 -0400

    Update and rename Ziyue-code.R to Ziyue_binary variables.R

--- 2_Analysis/Ziyue_binary variables.R
+++ 2_Analysis/Ziyue_binary variables.R
@@ -11,18 +11,18 @@ control$OTHER_RACE <- ifelse(control$RACE == 7, 1, 0)
 control$MULTIPLE_RACE <- ifelse(control$RACE %in% c(8,9), 1, 0)
 
 #Binary variable for married people
-control$MARRIED <- factor(control$MARST %in% c(1,2), 1, 0)
+control$MARRIED <- ifelse(control$MARST %in% c(1,2), 1, 0)
 
 #Binary variable for employed people
-control$EMPLOYED <- factor(control$EMPSTAT == 1, 1, 0)
+control$EMPLOYED <- ifelse(control$EMPSTAT == 1, 1, 0)
 
 #Binary variable for not in labor force
-control$NOTLABOR <- factor(control$EMPSTAT == 3, 1, 0)
+control$NOTLABOR <- ifelse(control$EMPSTAT == 3, 1, 0)
 
 #Dummy variables for numbers of generation in the family
-control$ONE_GEN <- factor(control$MULTGEN == 1, 1, 0)
-control$TWO_GEN <- factor(control$MULTGEN == 2, 1, 0)
-control$THREE_MORE_GEN <- factor(control$MULTGEN == 3, 1, 0)
+control$ONE_GEN <- ifelse(control$MULTGEN == 1, 1, 0)
+control$TWO_GEN <- ifelse(control$MULTGEN == 2, 1, 0)
+control$THREE_MORE_GEN <- ifelse(control$MULTGEN == 3, 1, 0)
 
 
 
